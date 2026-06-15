164bb2b68f956d0408ce143933f38890081a1fa7
GitHUB_DP <marmelaxx@gmail.com>
2026-06-15 15:32:17 +0300

    Prevent Streamlit rerun on downloads

--- app.py
+++ app.py
@@ -11,4 +11,24 @@ try:
 except Exception:
     pass
 
+# Streamlit Cloud can rerun the whole app after a download click.
+# For large Avaya reports this may freeze the frontend or lead to a black screen.
+# Make downloads ignore rerun when the installed Streamlit version supports it.
+try:
+    import streamlit as st
+
+    _original_download_button = st.download_button
+
+    def _download_button_without_rerun(*args, **kwargs):
+        kwargs.setdefault("on_click", "ignore")
+        try:
+            return _original_download_button(*args, **kwargs)
+        except TypeError:
+            kwargs.pop("on_click", None)
+            return _original_download_button(*args, **kwargs)
+
+    st.download_button = _download_button_without_rerun
+except Exception:
+    pass
+
 from app_shift_unified_v3 import *  # noqa: F401,F403
